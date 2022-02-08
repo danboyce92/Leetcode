4e8ec567203ccf364898bd08d397b84ea2d23043
danboyce92 <danboyce92@yahoo.com>
2022-02-08 18:19:37 +0100

    1st Leetcode

---
+++ TwoSum.js
@@ -0,0 +1,35 @@
+/*
+Given an array of integers nums and an integer target,
+return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution,
+ and you may not use the same element twice.
+
+You can return the answer in any order.
+
+_____________________________________
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+
+*/
+
+
+const twoSum = (nums, target) => {
+    let newArray = [];
+
+    for(let n1 = 0; n1 < nums.length; n1++) {
+
+        for(let n2 = n1 + 1; n2 < nums.length; n2++) {
+            if(nums[n1] + nums[n2] == target) {
+                newArray.push(n1);
+                newArray.push(n2);
+            }
+
+        }
+    }
+    return newArray;
+};
